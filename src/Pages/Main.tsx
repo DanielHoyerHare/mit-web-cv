d88aea959ad321059c4a9e8fb933175a73470148
DanielHoyerHare <danielhoyerhare@gmail.com>
2025-05-15 12:34:52 +0200

    Tilføjet billede til forside

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -5,6 +5,10 @@ import '../Global.css';
 function Main() {
     return (
         <div className="page-container border-radius box-shadow">
+            <div className="page-header">
+                <h1>Welcome to the React App</h1>
+                <img className="pfp" src="/img/pfp.png" alt="English" />
+            </div>
             <h1>Welcome to the React App</h1>
             <p className="testMargin">This is a simple React application.</p>
             <p className="testMargin">Feel free to explore the components and styles.</p>
